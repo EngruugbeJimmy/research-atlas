2234516a6b6bd622575748ae8bb597c4757a82b6
engrugbejimmy <james.ugbedeojoabah@gmail.com>
2026-07-24 05:56:34 +0300

    atlas ai gogle gemini model add

--- app/api/ask-atlas/route.ts
+++ app/api/ask-atlas/route.ts
@@ -81,7 +81,7 @@ export async function POST(req: NextRequest) {
     });
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash-lite",
+      model: "gemini-2.5-pro"
       contents: `${SYSTEM_PROMPT}
 
 User Question:
